f3b1afd761d711b5bd626ccf670a2226f18aa180
Shashwat Singh <shashwat123student@gmail.com>
2023-10-01 21:22:42 +0530

    Done with the basic Routing

---
+++ Shashwat_Singh/Anime_FavList/src/AddAnime.jsx
@@ -0,0 +1,9 @@
+function AddAnime(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default AddAnime;
---
+++ Shashwat_Singh/Anime_FavList/src/ShowAnime.jsx
@@ -0,0 +1,10 @@
+function ShowAnime(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default ShowAnime;
+
